41c3a9df6b40ddcb5a2af42890db0abe7a9f99cf
obafemi <oludahunsiobafemi18@gmail.com>
2021-12-01 21:13:40 +0100

    general practise to understand the concept of lifecycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import React from 'react';
+import Lifecycle from './Lifecycle'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      reasonsToLive : 47,
+    }
+  }
+
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+      return (
+        {reasonsToLive : prevState.reasonsToLive + 1}
+      )
+    }, () => console.log(this.state.reasonsToLive))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        <Lifecycle text = '_hello' />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Lifecycle.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Lifecycle extends React.Component {
+    constructor (){ 
+        super();
+        this.state = {
+            message : '',
+            text : '',
+        }
+        console.log("This is the constructor!")
+    }
+    componentDidMount(){
+        console.log('This is componentDidMount')
+    }
+    componentWillUnmount() {
+        console.log("this is componentUnmount")
+    }
+    shouldComponentUpdate(){
+        console.log('this is shouldComponentUpdate');
+        return true;
+    }
+    render(){
+        console.log('This is the render method')
+        return (
+            <>
+                <button onClick = {() => this.setState(state => ({message : !state.message}) )}>
+                    Toggle Lifecycle
+                </button>
+
+                <button onClick = {() => this.setState (state => ({text : state.text + this.props.text }))}>
+                    Show in-built Text
+                </button>
+                
+                <h2>{this.state.message ? '...LIFECYCLE' : null }</h2>
+                <p>{this.state.text}</p>
+            </>
+            
+        )
+    }
+}
+
+export default Lifecycle;
