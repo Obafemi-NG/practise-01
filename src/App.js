516312dec77449c395c20852742905d850c8b4e1
obafemi <oludahunsiobafemi18@gmail.com>
2022-01-12 08:03:32 +0100

    made some few modifications to the lifecycle.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import React from 'react';
-import Lifecycle from './Lifecycle'
+// import React from 'react';
+// import Lifecycle from './Lifecycle'
+//
+// import logo from './logo.svg';
+// import './App.css';
+//
+//  class App extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       reasonsToLive : 47,
+//     }
+//   }
+//
+//   handleClick = () => {
+//     this.setState((prevState, prevProps) => {
+//       return (
+//         {reasonsToLive : prevState.reasonsToLive + 1}
+//       )
+//     }, () => console.log(this.state.reasonsToLive))
+//   }
+//
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//         <Lifecycle text = '_hello' />
+//         </header>
+//       </div>
+//     );
+//   }
+//
+// }
+//
+// export default App;
 
-import logo from './logo.svg';
-import './App.css';
+//Next line is an entirely different code for practising react routing
 
- class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      reasonsToLive : 47,
-    }
-  }
+import { Outlet, Link } from "react-router-dom";
 
-  handleClick = () => {
-    this.setState((prevState, prevProps) => {
-      return (
-        {reasonsToLive : prevState.reasonsToLive + 1}
-      )
-    }, () => console.log(this.state.reasonsToLive))
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        <Lifecycle text = '_hello' />
-        </header>
-      </div>
-    );
-  }
-  
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+//
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     < />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//The code below is totally diffenrent and use when i was practising react router.
+
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice"
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path = 'expenses' element = {<Expenses />} />
+        <Route path = 'invoices' element = {<Invoices />}>
+          <Route index element = {<main style = {{padding : '1rem'}}>
+              <h2>Select a specific invoice broo!</h2>
+            </main>} />
+          <Route path = ':invoiceId' element = {<Invoice/>}  />
+        </Route>
+        <Route path = '*' element  = { <main>
+          <p>
+            There is nothing to view here.
+          </p>
+        </main> } />
+      </Route>
+
+
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
